1d3c8a9b68dd78c49ebc0def475fd3037bfc4b0e
Henrik L. Njølstad <henjolstad@hotmail.com>
2020-12-08 09:37:36 +0100

    instikksortering

--- sorteringer/insertsort.py
+++ sorteringer/insertsort.py
@@ -1,9 +1,9 @@
-from manim.manim import *
+from manim import *
 
 
 class Insertions(Scene):
     def construct(self):
-        tall = [4, 1, 9, 11, 6, 3, 10, 2]
+        tall = [1, 4, 9, 7, 0, 6, 3, 8, 10, 2, 5]
         antall = len(tall)
 
         tallobj = []
@@ -28,20 +28,22 @@ class Insertions(Scene):
             *[gruppe[i] for i in range(antall)]
         )
         self.wait()
-
+        self.bring_to_front(gruppe[0])
+        self.play(ApplyMethod(gruppe[0][1].set_color, GREEN))
         for k in range(antall-1):
             # tempo = .5
             if k == 0 or k == 1:
                 tempo = 1
-            else:
+            elif k == 2 or k == 3:
                 tempo = 1/2
+            else:
+                tempo = 1/4
             for i in range(k,-1,-1):
-                print(i)
                 self.play(*[ApplyMethod(gruppe[j].shift, UP*1.5)
                             for j in range(i, i+2)], run_time=tempo)
-                self.wait(0.3)
+                self.wait(tempo)
                 if tall[i] > tall[i+1]:
-                    self.play(Indicate(gruppe[i][0]))
+                    self.play(Indicate(gruppe[i][0]), run_time=tempo)
                     dummy = tall[i]
                     tall[i] = tall[i+1]
                     tall[i+1] = dummy
@@ -54,13 +56,17 @@ class Insertions(Scene):
                     dummy = gruppe[i+1]
                     gruppe[i+1] = gruppe[i]
                     gruppe[i] = dummy
+                    if i == 0:
+                        self.bring_to_front(gruppe[i])
+                        self.play(ApplyMethod(gruppe[i][1].set_color, GREEN), run_time=tempo)
+
                 else:
-                    self.play(Indicate(gruppe[i+1][0]))
+                    self.play(Indicate(gruppe[i+1][0]), run_time=tempo)
                     self.play(*[ApplyMethod(gruppe[j].shift, DOWN*1.5)
                                 for j in range(i, i+2)], run_time=tempo)
-
+                    self.bring_to_front(gruppe[i+1])
+                    self.play(ApplyMethod(gruppe[i+1][1].set_color, GREEN), run_time=tempo)
+                    
                     break
-            self.bring_to_front(gruppe[k])
-            self.play(ApplyMethod(gruppe[k][1].set_color, GREEN), run_time=tempo)
-        self.play(ApplyMethod(gruppe[-1][1].set_color, GREEN), run_time=tempo)
+        self.play(ApplyMethod(gruppe[-1][1].set_color, GREEN))
         self.wait(2)
